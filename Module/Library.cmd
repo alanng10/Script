186c79286371ab3c7efef4e1c3be64919a0c1722
Alan Ng <aaabbbcccddd00001111@gmail.com>
2025-10-21 11:12:34 +0800

    Module

--- Module/Library.cmd
+++ Module/Library.cmd
@@ -5,5 +5,5 @@ set DotNetOutFold=..\Class\Out\net8.0
 
 echo Library %Module%
 pushd %DotNetOutFold%
-saber library %Module%-0.00.00
+set "PATH=C:\\Users\\aaabb\\Qt\\6.8.1\\llvm-mingw_64\\bin;C:\\Users\\aaabb\\Qt\\Tools\\llvm-mingw1706_64\\bin;%PATH%" & saber library %Module%
 popd
